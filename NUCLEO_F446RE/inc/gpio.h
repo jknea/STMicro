ae0cfcf6320ce3c4dbc822256dbbfa56b0dbf633
jknea <kneajames@.com>
2023-04-10 19:53:40 -0500

    updated gpio functions

--- NUCLEO_F446RE/inc/gpio.h
+++ NUCLEO_F446RE/inc/gpio.h
@@ -2,16 +2,12 @@
  ******************************************************************************
  * @file           : main.c
  * @author         : jknea
- * @brief          :
+ * @brief          : Provides the interface for STM32F446 GPIO pins
  ******************************************************************************
  *
  * The GPIOs (general purpose input/outputs) can sink or source up to +-8 mA, and sink or
  * source up to +-20 mA (with a relaxed VOL/VOH) except PC13, PC14 and PC15 which can
  * sink or source up to +-3mA.
- *
- *
- *
- *
  *
  ******************************************************************************
  */
@@ -24,9 +20,10 @@
 #define BIT0_MASK 	0b01
 #define BIT1_MASK 	0b10
 
-enum logics {LOW, HIGH};
-enum modes {INPUT, OUTPUT, ALTERNATE, ANALOG};
-enum gpio_pins {
+typedef enum {LOW, HIGH} logic_t;
+typedef enum {INPUT, OUTPUT, ALTERNATE, ANALOG} pinmode_t;
+typedef enum
+{
 	PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7, PA8, PA9, PA10, PA11, PA12, PA13, PA14, PA15,
 	PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8, PB9, PB10, PB11, PB12, PB13, PB14, PB15,
 	PC0, PC1, PC2, PC3, PC4, PC5, PC6, PC7, PC8, PC9, PC10, PC11, PC12, PC13, PC14, PC15,
@@ -35,9 +32,11 @@ enum gpio_pins {
 	PF0, PF1, PF2, PF3, PF4, PF5, PF6, PF7, PF8, PF9, PF10, PF11, PF12, PF13, PF14, PF15,
 	PG0, PG1, PG2, PG3, PG4, PG5, PG6, PG7, PG8, PG9, PG10, PG11, PG12, PG13, PG14, PG15,
 	PH0, PH1, PH2, PH3, PH4, PH5, PH6, PH7, PH8, PH9, PH10, PH11, PH12, PH13, PH14, PH15
-};
+} pin_t;
+
 
 /* Arduino Pins as STM32 Pins */
+/* Digital Pins */
 #define D0		PA3
 #define D1		PA2
 #define D2		PA10
@@ -54,7 +53,7 @@ enum gpio_pins {
 #define D13		PA5
 #define D14		PB9
 #define D15		PB8
-
+/* Analog Pins */
 #define A0		PA0
 #define A1		PA1
 #define A2		PA4
@@ -66,8 +65,9 @@ enum gpio_pins {
 
 
 /* Function Prototypes */
-void pinMode(uint8_t pin, uint8_t mode);
-void digitalWrite(uint8_t pin_name, uint8_t mode);
-
+void pinMode(pin_t pin, pinmode_t mode);
+void digitalWrite(pin_t pin_name, logic_t mode);
+void allPinMode(pinmode_t mode);
+void allDigitalWrite(logic_t logic);
 
 #endif /* GPIO_H_ */
--- NUCLEO_F446RE/src/gpio.c
+++ NUCLEO_F446RE/src/gpio.c
@@ -1,33 +1,46 @@
-/*
- * gpio.c
+/*! @file gpio.c
+ *  @brief Provides the implementation for STM32F446 GPIO pins
  *
- *  Created on: Mar 20, 2023
- *      Author: kneaj
+ *  Details.
  */
 
 #include "stm32f446inc/stm32f446xx.h"
 #include "inc/gpio.h"
-#include <stddef.h>
 
+/* functions hidden to user */
 uint8_t gpioPinNum(uint8_t stm32_pin);
 GPIO_TypeDef * gpioPinToPort(uint8_t stm32_pin);
-GPIO_TypeDef * gpio_ports[] = {GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOF, GPIOG, GPIOH};
 
-void gpioClockEnable(GPIO_TypeDef * GPIOx)
+
+GPIO_TypeDef * gpio_ports[] = {GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOF, GPIOG, GPIOH};
+uint8_t arduino_digitals[] = {D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15};
+
+
+void allPinMode(pinmode_t mode)
 {
-	if (GPIOx == GPIOA) { RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; return; }
-	if (GPIOx == GPIOB) { RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; return; }
-	if (GPIOx == GPIOC) { RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN; return; }
-	if (GPIOx == GPIOD) { RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; return; }
-	if (GPIOx == GPIOE) { RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN; return; }
-	if (GPIOx == GPIOF) { RCC->AHB1ENR |= RCC_AHB1ENR_GPIOFEN; return; }
-	if (GPIOx == GPIOG) { RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN; return; }
-	if (GPIOx == GPIOH) { RCC->AHB1ENR |= RCC_AHB1ENR_GPIOHEN; return; }
+	for (int i = 0; i < 14; i++)
+	{
+		pinMode(arduino_digitals[i], mode);
+	}
+}
+
+void allDigitalWrite(logic_t logic)
+{
+	for (int i = 0; i < 14; i++)
+	{
+		digitalWrite(arduino_digitals[i], logic);
+	}
 }
 
 
+void gpioClockEnable(pin_t stm32_pin)
+{
+	uint8_t port = stm32_pin / 16;
+	RCC->AHB1ENR |= (1UL << port);
+}
 
-void gpioSetAltFunc(uint8_t stm32_pin, uint32_t AFx)
+
+void gpioSetAltFunc(pin_t stm32_pin, uint32_t AFx)
 {
 	GPIO_TypeDef *GPIOx = gpioPinToPort(stm32_pin);
 	uint8_t pnum = gpioPinNum(stm32_pin);
@@ -37,44 +50,37 @@ void gpioSetAltFunc(uint8_t stm32_pin, uint32_t AFx)
 }
 
 
-
-GPIO_TypeDef * gpioPinToPort(uint8_t stm32_pin)
+GPIO_TypeDef * gpioPinToPort(pin_t stm32_pin)
 {
-	return gpio_ports[stm32_pin / 16];  // will return 0 for port A, 
+	return gpio_ports[stm32_pin / 16];  // will return 0 for port A, 1 for port b
 }
 
 
-/*
- * Set the pin mode of an STM32 GPIO PIN
- * Modes are : {INPUT, OUTPUT, ALTERNATE, ANALOG}
- */
-void pinMode(uint8_t stm32_pin, uint8_t mode)
+/* Set the pin mode of an STM32 GPIO PIN. Modes are : {INPUT, OUTPUT, ALTERNATE, ANALOG} */
+void pinMode(pin_t stm32_pin, pinmode_t mode)
 {
+	gpioClockEnable(stm32_pin);
 	GPIO_TypeDef * GPIOx = gpioPinToPort(stm32_pin);
-	gpioClockEnable(GPIOx);
 	uint8_t pin_num = gpioPinNum(stm32_pin);
 	uint32_t lowbit = mode & BIT0_MASK;
 	uint32_t highbit = mode & BIT1_MASK;
 	if (lowbit == 0) { GPIOx->MODER &= ~(1UL << (2*pin_num)); }
 	else { GPIOx->MODER |= (1UL << (2*pin_num)); }
-
 	if (highbit == 0) { GPIOx->MODER &= ~(1UL << (2*pin_num + 1)); }
 	else { GPIOx->MODER |= (1UL << (2*pin_num + 1)); }
 }
 
 
-
-uint8_t gpioPinNum(uint8_t stm32_pin)
+uint8_t gpioPinNum(pin_t stm32_pin)
 {
 	return stm32_pin % 16;
 }
 
 
-
-void digitalWrite(uint8_t stm32_pin, uint8_t mode)
+void digitalWrite(pin_t stm32_pin, logic_t logic)
 {
 	GPIO_TypeDef *GPIOx = gpioPinToPort(stm32_pin);
 	uint8_t pin_num = gpioPinNum(stm32_pin);
-	if (mode == HIGH) { GPIOx->ODR |= (1UL << pin_num); }
+	if (logic == HIGH) { GPIOx->ODR |= (1UL << pin_num); }
 	else { GPIOx->ODR &= ~(1UL << pin_num); }
 }
